4e30640dfcb149dba8ad8ba3457db513738663cf
cienciaAplicadaComPython <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2021-12-23 10:05:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import matplotlib.pyplot as plt
 
+x = st.sidebar.slider('x')
+st.sidebar.write(x, 'squared is', x * x)
+
+y = x**2
+
+fig, ax = plt.subplots(figsize = (10,5))
+ax.scatter(x, y, label = 'Valores')
+plt.xlabel('$X$')
+plt.ylabel('$Y$')
+plt.legend()
+
+st.pyplot(fig)
