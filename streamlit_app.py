76ad617558c9dd6c8e0377d091a0b78314d60b12
cienciaAplicadaComPython <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2021-12-24 10:11:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,23 @@
 import streamlit as st
-import time
+import pandas as pd
+import numpy as np
 
-'Começando um longo cálculo computacional...'
+st.title('Uber pickups in NYC')
 
-# Adiciona um marcador
-ultima_iteracao = st.empty()
-barra = st.progress(0)
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-for i in range(100):
-  # Atualiza a barra de progresso a cada iteração
-  ultima_iteracao.text(f'Iteração {i+1}')
-  barra.progress(i + 1)
-  time.sleep(0.1)
-
-'...e agora está pronto!'
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+  
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
