6652a145b14a5dcfddc0418e370f3910c02f4b4b
cienciaAplicadaComPython <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2021-12-24 09:17:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,8 @@
-"""
-# Meu primeiro app
-Uma primeira tentativa para escrever uma tabela
-"""
-
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
