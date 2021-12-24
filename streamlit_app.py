e165cc947f30da5278e85171503adb240303f890
cienciaAplicadaComPython <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2021-12-24 09:23:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.write("A primeira tentativa para usar dados para criar uma tabela:")
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
 
 st.table(df)
