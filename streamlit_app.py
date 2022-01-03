65b6b3ba058e723f00a006e30c1c9df7ce7e3dd3
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2022-01-03 09:56:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,28 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.title('Uber pickups in NYC')
+st.set_page_config(page_title='Ciência Aplicada com Python', page_icon = 'random')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+topico = st.sidebar.radio(
+    'Escolha um tópico:',
+    (
+        'Introdução',
+        'Medição',
+        'Mecânica'
+    ),
+)
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-  
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
+if topico == 'Introdução':
+         """
+         # Intrdoução
+         
+         Está aplicação WEB contém uma coleção de artigos com respostas baseadas na ciência à perguntas feitas livremente.
+         Muitas delas são inspiradas em leituras, filmes, séries, documentários e viagens.
+         Cada artigo contém um algoritmo escrito com base na linguagem de programação *ython* que permite a utilização da solução proposta de maneira prática.
+         
+         Como objetivo, espera-se aumentar o interesse do leitor pela ciência, estimular a sua natureza investigativa e apoiar a integração entre a ciência e computação.
+         
+         *Obrigado por visitar **Ciência Aplicada com Python***
+         """
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-st.subheader('Map of all pickups')
-st.map(data)
-
-hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
+st.caption('**Desenvolvido por**: Jefferson Silva')
+st.caption('**E-mail**: j.davidss@hotmail.com')
