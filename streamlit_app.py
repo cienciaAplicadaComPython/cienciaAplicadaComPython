ddb65b162a79b281c53232006e3139260f876e82
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2022-01-03 10:17:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,11 +29,11 @@ elif topico == 'Medição':
     topicoDeMedicao = st.sidebar.radio(
         'Escolha um tópico:',
         (
-            'Quanto tempo dura uma ultrapassagem?'
+            "Quanto tempo dura uma ultrapassagem?"
         ),
     )
     
-    if topicoDeMedicao == 'Quanto tempo dura uma ultrapassagem?':
+    if topicoDeMedicao == "Quanto tempo dura uma ultrapassagem?":
         """
         # Em desenvolvimento
         """
