599f955855e78eb27225dbc06f1561db8e07fc66
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2022-01-03 12:01:57 -0300

    Update comoConverterMedidasEntreDiferentesSistemas.py

--- Medicao/comoConverterMedidasEntreDiferentesSistemas.py
+++ Medicao/comoConverterMedidasEntreDiferentesSistemas.py
@@ -1,4 +1,5 @@
 textoDoArtigo = """
+  
   # Como converter medidas entre sistemas diferentes?
   
   *Como converter as polegadas em metros ou vice-versa? O que a medida onças representa?
@@ -6,9 +7,11 @@ textoDoArtigo = """
   Abaixo você encontra as respostas para todas essas perguntas.*
   
   Tempo de leitura: 5 minutos
-  ---------------------------
+  
+  ---
   
   ## Introdução
+  
   Realizamos medições quando queremos caracterizar um objeto, uma pessoa ou um animal por meio da comparação com padrões conhecidos de comprimento, massa, volume e outros.
   Podemos ainda avaliar o desempenho de atividades com o uso de padrões de velocidade, potências e outros.
   
@@ -17,6 +20,7 @@ textoDoArtigo = """
   - Sistema Imperial de Unidades
   
   ## O Sistema internacional de unidades
+  
   Em 1971, na 14<sup>a</sup> Conferência Geral de Pesos e Medidas, sete grandezas foram selecionadas como fundamentais para constituir a base do SI:
   
   |Grandeza                 |Nome da Unidade    |Símbolo da Unidade   |
