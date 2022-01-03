1533875e2c2eeac51718bb187d127591f4b51283
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2022-01-03 12:14:33 -0300

    Update comoConverterMedidasEntreDiferentesSistemas.py

--- Medicao/comoConverterMedidasEntreDiferentesSistemas.py
+++ Medicao/comoConverterMedidasEntreDiferentesSistemas.py
@@ -42,24 +42,24 @@ textoDoArtigo = """
   
   Este sistema foi primeiramente definido no ato britânico de 1824 de pesos e medidas e ele pode ser dividido em unidades de comprimento, área, volume e massa:
   
-  |Nome da Unidade  |Símbolo da Unidade |Equivalência com o SI          |
-  |:--              |:--:               |                            --:|              
-  |                 |Comprimento        |                               |
-  |Polegada         |in                 |0,0254 m                       |
-  |Pés              |ft                 |0,3048 m                       |
-  |Jardas           |yd                 |0,9144 m                       |
-  |Milha            |mi                 |1.609,344 m                    |
-  |                 |Área               |                               |
-  |Acre             |mi                 |4.046,86 m<sup>2</sup>         |
-  |Milha quadrada   |sq mi              |2.589.988,110336 m<sup>2</sup> |
-  |                 |Volume             |                               |
-  |Onças            |oz                 |0,0000295735 m<sup>3</sup>     |
-  |Pinta            |pt                 |0,000473176 m<sup>3</sup>      |
-  |Galão            |gal                |0,00378541 m<sup>3</sup>       |
-  |                 |Massa              |                               |
-  |Libras           |lb                 |0,4536 kg                      |
-  |Stones           |st                 |6,3503 kg                      |
-  |Tonelada         |ton                |1.016,0469 kg                  |
+  |Nome da Unidade  |Símbolo da Unidade   |Equivalência com o SI          |
+  |:--              |:--:                 |                            --:|              
+  |                 |*Comprimento*        |                               |
+  |Polegada         |in                   |0,0254 m                       |
+  |Pés              |ft                   |0,3048 m                       |
+  |Jardas           |yd                   |0,9144 m                       |
+  |Milha            |mi                   |1.609,344 m                    |
+  |                 |*Área*               |                               |
+  |Acre             |mi                   |4.046,86 m<sup>2</sup>         |
+  |Milha quadrada   |sq mi                |2.589.988,110336 m<sup>2</sup> |
+  |                 |*Volume*             |                               |
+  |Onças            |oz                   |0,0000295735 m<sup>3</sup>     |
+  |Pinta            |pt                   |0,000473176 m<sup>3</sup>      |
+  |Galão            |gal                  |0,00378541 m<sup>3</sup>       |
+  |                 |*Massa*              |                               |
+  |Libras           |lb                   |0,4536 kg                      |
+  |Stones           |st                   |6,3503 kg                      |
+  |Tonelada         |ton                  |1.016,0469 kg                  |
 
 
   """
