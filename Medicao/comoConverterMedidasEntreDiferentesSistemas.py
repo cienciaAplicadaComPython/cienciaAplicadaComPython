1f9646f75a1ff01aca573d90a81983a9efde0bb5
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2022-01-03 12:12:28 -0300

    Update comoConverterMedidasEntreDiferentesSistemas.py

--- Medicao/comoConverterMedidasEntreDiferentesSistemas.py
+++ Medicao/comoConverterMedidasEntreDiferentesSistemas.py
@@ -10,6 +10,7 @@ textoDoArtigo = """
   
   ---
   
+  
   ## Introdução
   
   Realizamos medições quando queremos caracterizar um objeto, uma pessoa ou um animal por meio da comparação com padrões conhecidos de comprimento, massa, volume e outros.
@@ -18,6 +19,7 @@ textoDoArtigo = """
   Os padrões internacionais são resultados de acordos entre cientistas de diferentes países sobre os procedimentos necessários para a reprodução destes padrões e eles são agrupados em **Sistemas de Medidas**. Atualmente, os seguintes sistemas de medidas são utilizados:
   - Sistema Internacional de Unidades (SI)
   - Sistema Imperial de Unidades
+  
   
   ## O Sistema internacional de unidades
   
@@ -32,6 +34,32 @@ textoDoArtigo = """
   |Quantidade de substância |mol                |mol                  |
   |Densidade luminosa       |candela            |Cd                   |
   |Corrente elétrica        |ampere             |A                    |
+
+  Outras unidades são derivadas das unidades fundamentais do SI. Por exemplo, a grandeza força é representada pela unidade newton (N) a qual equivale a kg*x*m/s.
+  
+  
+  ## O sistema imperial de unidades
+  
+  Este sistema foi primeiramente definido no ato britânico de 1824 de pesos e medidas e ele pode ser dividido em unidades de comprimento, área, volume e massa:
+  
+  |Nome da Unidade  |Símbolo da Unidade |Equivalência com o SI          |
+  |:--              |:--:               |                            --:|              
+  |                 |Comprimento        |                               |
+  |Polegada         |in                 |0,0254 m                       |
+  |Pés              |ft                 |0,3048 m                       |
+  |Jardas           |yd                 |0,9144 m                       |
+  |Milha            |mi                 |1.609,344 m                    |
+  |                 |Área               |                               |
+  |Acre             |mi                 |4.046,86 m<sup>2</sup>         |
+  |Milha quadrada   |sq mi              |2.589.988,110336 m<sup>2</sup> |
+  |                 |Volume             |                               |
+  |Onças            |oz                 |0,0000295735 m<sup>3</sup>     |
+  |Pinta            |pt                 |0,000473176 m<sup>3</sup>      |
+  |Galão            |gal                |0,00378541 m<sup>3</sup>       |
+  |                 |Massa              |                               |
+  |Libras           |lb                 |0,4536 kg                      |
+  |Stones           |st                 |6,3503 kg                      |
+  |Tonelada         |ton                |1.016,0469 kg                  |
 
 
   """
