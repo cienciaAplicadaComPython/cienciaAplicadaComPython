e6bf6fff77d287ae1398684c5899d10c18b5d083
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2022-01-03 11:41:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import medicao.comoConverterMedidasEntreDiferentesSistemas as comoConverterMedidasEntreDiferentesSistemas
+import comoConverterMedidasEntreDiferentesSistemas
 
 st.set_page_config(page_title='Ciência Aplicada com Python', page_icon = 'random')
 
