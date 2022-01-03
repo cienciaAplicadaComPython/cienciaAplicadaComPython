2d914c8bfd6f6da1d7c642b313e278d8deefa0c7
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2022-01-03 11:58:54 -0300

    Update comoConverterMedidasEntreDiferentesSistemas.py

--- Medicao/comoConverterMedidasEntreDiferentesSistemas.py
+++ Medicao/comoConverterMedidasEntreDiferentesSistemas.py
@@ -1,3 +1,33 @@
 textoDoArtigo = """
-  # Como converter medidas entre sistemas diferentes?
+  #Como converter medidas entre sistemas diferentes?
+  
+  *Como converter as polegadas em metros ou vice-versa? O que a medida onças representa?
+  Quais sistemas de medidas existem?
+  Abaixo você encontra as respostas para todas essas perguntas.*
+  
+  Tempo de leitura: 5 minutos
+  ---------------------------
+  
+  ##Introdução
+  Realizamos medições quando queremos caracterizar um objeto, uma pessoa ou um animal por meio da comparação com padrões conhecidos de comprimento, massa, volume e outros.
+  Podemos ainda avaliar o desempenho de atividades com o uso de padrões de velocidade, potências e outros.
+  
+  Os padrões internacionais são resultados de acordos entre cientistas de diferentes países sobre os procedimentos necessários para a reprodução destes padrões e eles são agrupados em **Sistemas de Medidas**. Atualmente, os seguintes sistemas de medidas são utilizados:
+  - Sistema Internacional de Unidades (SI)
+  - Sistema Imperial de Unidades
+  
+  ##O Sistema internacional de unidades
+  Em 1971, na 14<sup>a</sup> Conferência Geral de Pesos e Medidas, sete grandezas foram selecionadas como fundamentais para constituir a base do SI:
+  
+  |Grandeza                 |Nome da Unidade    |Símbolo da Unidade   |
+  |:--                      |:--:               |                  --:|
+  |Comprimento              |metro              |m                    |
+  |Massa                    |kilograma          |kg                   |
+  |Tempo                    |segundo            |s                    |
+  |Temperatura              |kelvin             |K                    |
+  |Quantidade de substância |mol                |mol                  |
+  |Densidade luminosa       |candela            |Cd                   |
+  |Corrente elétrica        |ampere             |A                    |
+
+
   """
