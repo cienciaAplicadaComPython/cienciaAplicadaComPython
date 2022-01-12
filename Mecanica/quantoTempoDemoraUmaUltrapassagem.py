92987b937f505ae562688871a45b857e9e8014ef
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2022-01-12 11:21:32 -0300

    Update quantoTempoDemoraUmaUltrapassagem.py

--- Mecanica/quantoTempoDemoraUmaUltrapassagem.py
+++ Mecanica/quantoTempoDemoraUmaUltrapassagem.py
@@ -18,15 +18,28 @@ Então vamos à resposta!
 ## Modelo
 
 Recorreremos ao seguinte modelo para encontrar a resposta:
- - Dois veículos, $C1$ e $C2$, movem-se com velocidade constante na mesma direção e sentido, da esquerda para a direita.
+ - Dois veículos, $C_1$ e $C_2$, movem-se com velocidade constante na mesma direção e sentido, da esquerda para a direita.
  Chamemos este cenário de situação inicial.
- Os veículos $C1$ e $C2$ têm comprimento $d_{c1}$ e $d_{c2}$, respectivamente. A distância entre os veículos $C1$ e $C2$ é $d_{c1c2}$.'''
+ Os veículos $C_1$ e $C_2$ têm comprimento $d_{c_1}$ e $d_{c_2}$, respectivamente. A distância entre os veículos $C_1$ e $C_2$ é $d_{c_1c_2}$.'''
 
-continuacaoDaIntroducaoDoArtigo = '''Após a ultrapassagem começar, vamos considerar que ela termina quando o veículo $C1$ estiver à frente do veículo $C2$ de uma distância igual a $d_{c1c2}$.
+continuacaoDaIntroducaoDoArtigo = '''Após a ultrapassagem começar, vamos considerar que ela termina quando o veículo $C_1$ estiver à frente do veículo $C_2$ de uma distância igual a $d_{c_1c_2}$.
 Chamemos este cenário de situação final:'''
 
 conclusaoDaIntroducaoDoArtigo = '''Vamos desconsiderar o tempo que o carro leva para entrar na contramão, o atrito do asfalto, a resistência do vento e outras variáveis.
-Assumindo então que os veículos movem-se em linha reta e com velocidade constante, da análise do movimento retilíneo uniforme temos a seguinte fórmula $$v = \frac{d}{t}$$ na qual $v$, $d$ e $t$ representam a velocidade, a distância e o tempo, respectivamente. Aqui, vamos considerar somente a velocidade escalar.
-Como desejamos encontrar o tempo durante o qual é realizada a ultrapassagem, ou seja $t$, e é necessário que o veículo C1 esteja com uma velocidade superior ao do veículo C2, vamos considerar a diferença entre as velocidades dos dois veículos, $\Delta v$, ao invés de $v$.
-Logo, para o veículo C1 percorrrer uma distância de $d_{c_1c_2} + d_{c_2} + d_{c_1c_2} + d_{c_1}$ sendo que a sua velocidade é superior a velocidade do veículo C2 por $\Delta v$, será necessário um tempo de aproximadamente $$t = \frac{d_{c_1} + d_{c_2} + 2 \times d_{c_1c_2}}{\Delta v}$$
-Ao utilizar a fórmula acima, sempre observe as unidades! Caso você deseje utilizar $\Delta v$ em km/h, será preciso converter os valores de distância para km. Neste caso, o resultado '''
+
+Assumindo então que os veículos movem-se em linha reta e com velocidade constante, da análise do movimento retilíneo uniforme temos a seguinte fórmula
+
+$v = \frac{d}{t}$
+
+na qual $v$, $d$ e $t$ representam a velocidade, a distância e o tempo, respectivamente.
+Aqui, vamos considerar somente a velocidade escalar.
+Como desejamos encontrar o tempo durante o qual é realizada a ultrapassagem, ou seja $t$, e é necessário que o veículo $C_1$ esteja com uma velocidade superior ao do veículo $C_2$, vamos considerar a diferença entre as velocidades dos dois veículos, $\Delta v$, ao invés de $v$.
+Logo, para o veículo $C_1$ percorrrer uma distância de $d_{c_1c_2} + d_{c_2} + d_{c_1c_2} + d_{c_1}$ sendo que a sua velocidade é superior a velocidade do veículo $C_2$ por $\Delta v$, será necessário um tempo de aproximadamente
+
+$t = \frac{d_{c_1} + d_{c_2} + 2 \times d_{c_1c_2}}{\Delta v}$
+
+Ao utilizar a fórmula acima, sempre observe as unidades!
+Caso você deseje utilizar $\Delta v$ em km/h, será preciso converter os valores de distância para km. Neste caso, o resultado obtido para o tempo será em horas.
+Para utilizar os valores comumente pensados na prática, ou seja, a diferença de velocidades em km/h e as distâncias em m e obter o tempo de ultrapassagem em s, podemos utilizar a seguinte equação:
+
+$t = \frac{\frac{d_{c_1} + d_{c_2} + 2 \times d_{c_1c_2}\; \text{km}}{1000}}{\Delta v\; \text{km/h}} \times \frac{3.600\; \text{s}}{\text{h}} \\ t = \frac{d_{c_1} + d_{c_2} + 2 \times d_{c_1c_2}}{\Delta v} \times 3,6\; \text{s}$'''
