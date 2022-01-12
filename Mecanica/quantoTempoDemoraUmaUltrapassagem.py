ba9acf1dce3a4a08d5ebc70f6a610095e81a4e5f
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2022-01-12 10:45:18 -0300

    Create quantoTempoDemoraUmaUltrapassagem.py

---
+++ Mecanica/quantoTempoDemoraUmaUltrapassagem.py
@@ -0,0 +1,23 @@
+introducaoDoArtigo = '''# Quanto tempo demora uma ultrapassagem?
+
+*Sabe dizer quanto tempo você fica na pista contrária durante uma ultrapassagem?
+Você poderia trazer grande risco a você mesmo e aos outros passageiros!
+Entenda por meio deste exemplo de aplicação de física na vida real a dimensão desse risco.*
+
+Tempo de leitura: 8 minutos
+
+----------------------------
+
+## Introdução
+
+Se você já esteve na autoestrada, a manobra de ultrapassagem já se mostrou como uma alternativa para passar menos tempo nela.
+Porém, essa é uma manobra que traz riscos e ter controle sobre ela reduz eles.
+Se você souber dizer quanto tempo a ultrapassagem durará, você estará iniciando uma manobra com a vantagem de ter uma informação adicional sobre ela e isto fará você se sentir mais seguro.
+Então vamos à resposta!
+
+## Modelo
+
+Recorreremos ao seguinte modelo para encontrar a resposta:
+ - Dois veículos, $C1$ e $C2$, movem-se com velocidade constante na mesma direção e sentido, da esquerda para a direita.
+ Chamemos este cenário de situação inicial.
+ Os veículos $C1$ e $C2$ têm comprimento $d_{c1}$ e $d_{c2}$, respectivamente. A distância entre os veículos $C1$ e $C2$ é $d_{c1c2}$.'''
